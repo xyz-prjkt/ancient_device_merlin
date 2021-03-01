61aac6911082f3d2ff565f410addbd155aeff180
xyzuan <i.xyzuan@gmail.com>
2021-03-01 10:27:05 +0000

    merlin: overlay-ancient: define Redmi Note 9 specs.

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-ancient
 
 # Dynamic Partitions 
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
