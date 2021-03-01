b70322ac07afbceec2eb38f4fc0bbfc7695c9ce3
Jody Yuantoro <i.xyzuan@gmail.com>
2021-03-01 14:51:39 +0700

    merlin: add setup.sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/xyz-prjkt/vendor_mediatek_ims vendor/mediatek/ims -b 11 && git clone https://github.com/xyz-prjkt/vendor_mediatek_interfaces vendor/mediatek/interfaces -b 11
